fee15fe41bd50e16913c5ace9ca342b4ffdcb130
Newbiever2 <iidea47949s@gmail.com>
2026-03-07 21:50:13 +0700

    AddFixCategory/Image

--- WebApplicationASP/WebApplicationASP/obj/Debug/net10.0/WebApplicationASP.AssemblyInfo.cs
+++ WebApplicationASP/WebApplicationASP/obj/Debug/net10.0/WebApplicationASP.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplicationASP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+946d4f72a6c219c8a966829f17169b0b77a494fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a4a457da0182b5eb2036f035caa2ba85484f938")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplicationASP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplicationASP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
