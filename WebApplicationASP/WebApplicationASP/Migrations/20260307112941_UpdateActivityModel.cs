cd55ad8139b4a332f1d59b275e5fd966dcbeef06
Appalux <tenjigay123@gmail.com>
2026-03-07 20:13:47 +0700

    AddActivity

---
+++ WebApplicationASP/WebApplicationASP/Migrations/20260307112941_UpdateActivityModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplicationASP.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateActivityModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Number",
+                table: "Activities",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Number",
+                table: "Activities");
+        }
+    }
+}
--- WebApplicationASP/WebApplicationASP/Models/ActivityModel.cs
+++ WebApplicationASP/WebApplicationASP/Models/ActivityModel.cs
@@ -1,19 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace WebApplicationASP.Models
 {
     public class Activity
     {
         public int Id { get; set; }
-
         public string Title { get; set; }
-
         public string Description { get; set; }
-
-        public string Category { get; set; }
-
-        public string Status { get; set; }
-
-        public string ImageUrl { get; set; }
-
         public DateTime ExpireDate { get; set; }
+        public int Number { get; set; }
+        public List<string> Category { get; set; }
+        public string Status { get; set; }
+        public string ImageUrl { get; set; }
+        public List<string> Member { get; set; }
+
     }
 }
