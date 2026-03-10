abc96ceef00f774abe92dbd8119ca88f98dd1f3d
DEKDDD <supawe2548@gmail.com>
2026-03-11 03:25:47 +0700

    yay

--- WebApplicationASP/WebApplicationASP/obj/Debug/net10.0/WebApplicationASP.AssemblyInfo.cs
+++ WebApplicationASP/WebApplicationASP/obj/Debug/net10.0/WebApplicationASP.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplicationASP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bafb47669d1bf5f1c31b0e99c8a9be8fce78e9ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00b4ba1ab2392a5ec51664047f63b02504bd675f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplicationASP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplicationASP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
